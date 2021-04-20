a2b330c17afb89f2a6e281692384ab8813530434
AggrievedBubble <jacob.neave1@gmail.com>
2021-04-20 09:29:38 +0100

    window manager and main window created. not yet functional

---
+++ src/salesmanager/SalesManager.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salesmanager;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author j9neave
+ */
+public class SalesManager {
+	
+	private JFrame MainWindow;
+	
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		JFrame MainWindow = new MainWindow();
+		
+		
+	}
+	
+	public JFrame getMainWindow() {
+		return MainWindow;
+	}
+	
+}
